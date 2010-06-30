e52107bc1328222a4b8111975304daec3aa519c9
jaymz <jaymz@mail.u-dox.com>
2010-06-30 13:37:53 +0100

    posts rendering out basically, wrapper on list view

--- urls.py
+++ urls.py
@@ -2,7 +2,8 @@ from django.conf.urls.defaults import patterns
 from blogger.models import BloggerBlog
 
 urlpatterns = patterns('',
-    (r'^(?P<slug>[\w-]+)$', 'django.views.generic.list_detail.object_list', {
-            'queryset': BloggerBlog.objects.all(),
-        }, 'blog'),
+    (r'^(?P<slug>[\w-]+)/$', 'blogger.views.blog', {}, 'blog'),
 )
+
+
+
--- views.py
+++ views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.views.generic.list_detail import object_list
+from blogger.models import BloggerBlog, BloggerPost
+
+def blog(request, slug):
+
+    return object_list(request, BloggerPost.objects.all())
