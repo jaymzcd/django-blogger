38337bbef6609d1da7720c22e3e7a8d00e59d3be
jaymz <jaymz@mail.u-dox.com>
2010-06-30 14:35:52 +0100

    view/urls to list out based on slug & pk for blogs

--- urls.py
+++ urls.py
@@ -2,7 +2,8 @@ from django.conf.urls.defaults import patterns
 from blogger.models import BloggerBlog
 
 urlpatterns = patterns('',
-    (r'^(?P<slug>[\w-]+)/$', 'blogger.views.blog', {}, 'blog'),
+    (r'^(?P<blog_id>\d+)/$', 'blogger.views.blog_via_pk', {}, 'blog_pk'),
+    (r'^(?P<slug>[\w-]+)/$', 'blogger.views.blog_via_slug', {}, 'blog_slug'),
 )
 
 
--- views.py
+++ views.py
@@ -1,6 +1,13 @@
 from django.views.generic.list_detail import object_list
+from django.shortcuts import get_object_or_404
 from blogger.models import BloggerBlog, BloggerPost
 
-def blog(request, slug):
+def blog_via_slug(request, slug):
+    _slug = slug # this always feels ugly
+    blog = get_object_or_404(BloggerBlog, slug=_slug)
+    return object_list(request, blog.posts)
 
-    return object_list(request, BloggerPost.objects.all())
+def blog_via_pk(request, blog_id):
+    blog = get_object_or_404(BloggerBlog, pk=blog_id)
+    return object_list(request, blog.posts)
+
